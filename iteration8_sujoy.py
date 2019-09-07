4c9afbbb8487582f883b14cb2565944b96bccde8
Somnath Giri <somnathgiri1@outlook.com>
2019-09-07 22:25:34 +0530

    Sujoy created a python script in iteration8

---
+++ iteration8_sujoy.py
@@ -0,0 +1,10 @@
+
+--- python programing 
+x=int(input("Please enter 1st number: "))
+y=int(input("Please enter 2nd number: "))
+z=int(input("Please enter 3rd number: "))
+
+print (max(x,y,z))
+
+input("Please press enter to exit")
+
